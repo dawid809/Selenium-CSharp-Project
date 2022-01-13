ac960a31d8193e1039c7b336197398e1a6cd0934
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2022-01-13 13:56:15 +0100

    First part done

---
+++ Selenium-CSharp-Project/BaseClass/BaseTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Selenium_CSharp_Project.BaseClass
+{
+   public class BaseTest
+   {
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.facebook.com/";
+        }
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+    }
+}
