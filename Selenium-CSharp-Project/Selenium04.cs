9ae5b56bd4f68f2071a07b390de90f049423186a
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2022-01-16 17:16:44 +0100

    Fourth part done

---
+++ Selenium-CSharp-Project/Selenium04.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_CSharp_Project
+{
+    [TestFixture]
+    public class Selenium04
+    {
+        [Test]
+        [Author("Dawid", "dawid123@gmail.com")]
+        [Description("Facebook login verify")]
+        [TestCaseSource("DataDrivenTesting")]
+        public void Test1(string urlName)
+        {
+            IWebDriver driver =null;
+            try
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                //driver.Url = "https://www.facebook.com/";
+                driver.Url = urlName;
+                //IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+                IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='abcd']"));
+                emailTextField.SendKeys("Selenium C#");
+                driver.Quit();
+            }
+            catch (Exception exc)
+            {
+                ITakesScreenshot ts = driver as ITakesScreenshot;
+                Screenshot screenshot = ts.GetScreenshot();
+                screenshot.SaveAsFile("C:\\Users\\PC\\source\\repos\\Selenium-CSharp\\Selenium-CSharp-Project\\Screenshots\\ss1.jpeg", ScreenshotImageFormat.Jpeg);
+                Console.WriteLine(exc.StackTrace);
+                throw;
+            }
+            finally
+            {
+                if(driver != null)
+                {
+                    driver.Quit();
+                }
+            }
+        }
+
+        static IList DataDrivenTesting()
+        {
+            ArrayList list = new ArrayList();
+            list.Add("https://www.facebook.com/");
+            //list.Add("https://www.youtube.com/");
+            //list.Add("https://www.gmail.com/");
+            return list;
+        }
+
+        //[Test]
+        //[Author("Dawid", "dawid123@gmail.com")]
+        //[Description("Facebook login verify")]
+        //public void Test2()
+        //{
+        //    IWebDriver driver = new ChromeDriver();
+        //    driver.Manage().Window.Maximize();
+        //    driver.Url = "https://www.facebook.com/";
+        //    IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+        //    emailTextField.SendKeys("Selenium C#");
+        //    driver.Quit();
+        //}
+    }
+}
