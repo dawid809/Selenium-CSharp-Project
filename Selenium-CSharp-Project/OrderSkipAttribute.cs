8336733b40c654d58456454d71106c2bb4b611ad
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2022-01-16 16:17:36 +0100

    Third part done

---
+++ Selenium-CSharp-Project/OrderSkipAttribute.cs
@@ -0,0 +1,53 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Selenium_CSharp_Project.BaseClass;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+
+namespace Selenium_CSharp_Project
+{
+    [TestFixture]
+    public class OrderSkipAttribute
+    {
+
+        [Test, Order(2), Category("OrderSkipAttribute")]
+        public void TestMethod1()
+        {
+            // Ignore attribute
+            Assert.Ignore("Defect 12345");
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.facebook.com/";
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            driver.Close();
+        }
+
+        [Test, Order(1), Category("OrderSkipAttribute")]
+        public void TestMethod2()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.facebook.com/";
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            driver.Close();
+        }
+
+        [Test, Order(0), Category("OrderSkipAttribute")]
+        public void TestMethod3()
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Url = "https://www.facebook.com/";
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            driver.Close();
+        }
+    }
+}
---
+++ Selenium-CSharp-Project/ParallelTesting.cs
@@ -0,0 +1,115 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Selenium_CSharp_Project.BaseClass;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using Selenium_CSharp_Project.Utilities;
+
+namespace Selenium_CSharp_Project
+{
+    [TestFixture]
+    public class ParallelTesting
+    {
+        IWebDriver driver;
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod1()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailTextField = Driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Driver.Close();
+        }
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod2()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailTextField = Driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Driver.Close();
+        }
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod3()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailTextField = Driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Driver.Close();
+        }
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod4()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailTextField = Driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Driver.Close();
+        }
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod5()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailTextField = Driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Driver.Close();
+        }
+
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod6()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailTextField = Driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Driver.Close();
+        }
+
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod7()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailTextField = Driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Driver.Close();
+        }
+
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod8()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailTextField = Driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Driver.Close();
+        }
+
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod9()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailTextField = Driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Driver.Close();
+        }
+
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod10()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailTextField = Driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Driver.Close();
+        }
+    }
+}
